c5be17219582f4d125e808ed586c5645e11d2f8f
eunjaelim <imeunjae361@gmal.com>
2024-06-24 16:54:37 +0900

    PRJ3

---
+++ src/quiz/Quiz1.java
@@ -0,0 +1,18 @@
+package quiz;
+
+public class Quiz1 {
+
+	public static void main(String[] args) {
+		int num = -10; // -10
+		int result = +num; // -10
+		
+		int num2 = -20; // -20
+		int result2 = - num2; // 20
+		
+		System.out.println(num);
+		System.out.println(result);
+		System.out.println(num2);
+		System.out.println(result2);
+	}
+
+}
